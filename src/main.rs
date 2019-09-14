952352a8a53e4be81bbd16f9fbc3889df13cba35
Stefan Koch <programming@stefan-koch.name>
2019-09-14 19:00:03 +0200

    clone git repo from first input argument

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use std::env;
+
 fn main() {
-    // TODO: Entrypoint must be flexible
-    cinderella::run("https://github.com/aufziehvogel/CInderella.git")
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        cinderella::run(&args[1])
+    } else {
+        println!("Please specify the URL to a git repository");
+    }
 }
