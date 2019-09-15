aa1349f5901b3c257ec6fab18353bf045222c2f1
Stefan Koch <programming@stefan-koch.name>
2019-09-15 23:13:09 +0200

    add unit tests to configuration file reading

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -38,3 +38,36 @@ pub fn load_pipeline(path: &PathBuf) -> Option<Vec<Pipeline>> {
         None
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Write;
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn test_load_valid_config() {
+        let config = "[my-test]\ncommands = [\"echo Hallo\"]";
+        let mut tmpfile = NamedTempFile::new().unwrap();
+        let f = tmpfile.as_file_mut();
+        f.write_all(config.as_bytes()).expect("Unable to write to file");
+
+        let pipelines = load_pipeline(&tmpfile.path().to_path_buf());
+        let pipelines = pipelines.unwrap();
+
+        assert_eq!(pipelines.len(), 1);
+        assert_eq!(pipelines[0].name, "my-test");
+        assert_eq!(pipelines[0].commands.len(), 1);
+        assert_eq!(pipelines[0].commands[0], "echo Hallo");
+    }
+
+    #[test]
+    fn test_none_on_missing_config() {
+        let mut path = PathBuf::new();
+        path.push("/tmp/some/invalid/path/cinderella.toml");
+
+        let pipelines = load_pipeline(&path);
+
+        assert!(pipelines.is_none());
+    }
+}
